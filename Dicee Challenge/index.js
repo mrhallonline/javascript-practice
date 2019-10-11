272f12999b2d132acbe13e4a90067f206942b2f4
Kevin <mrhallonline@gmail.com>
2019-10-11 16:15:05 -0500

    finished one of the dice on the page, kept messing up the last conditional statement

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,26 @@
+// Random number generator.
+
+
+
+// change attribute of image source 
+
+    var randomNumber1 = Math.floor(Math.random()*6+1);
+
+    if (randomNumber1===1){
+        document.querySelector(".img1").setAttribute("src","images/dice1.png");
+    } 
+    else if (randomNumber1===2) {
+        document.querySelector(".img1").setAttribute("src","images/dice2.png");
+    } 
+    else if (randomNumber1===3){
+        document.querySelector(".img1").setAttribute("src","images/dice3.png");
+    } 
+    else if (randomNumber1===4){
+        document.querySelector(".img1").setAttribute("src","images/dice4.png");
+    } 
+    else if (randomNumber1===5){
+        document.querySelector(".img1").setAttribute("src","images/dice5.png");
+    } 
+    else {
+        document.querySelector(".img1").setAttribute("src","images/dice6.png");
+    }

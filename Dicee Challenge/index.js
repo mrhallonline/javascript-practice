d4d7925efceb47c1e506c7522c7a8c997173495f
Kevin <mrhallonline@gmail.com>
2019-10-11 17:04:59 -0500

    completed my version

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,11 +1,8 @@
 // Random number generator.
-
-
-
+var randomNumber1 = Math.floor(Math.random()*6+1);
+var randomNumber2 = Math.floor(Math.random()*6+1);
 // change attribute of image source 
-
-    var randomNumber1 = Math.floor(Math.random()*6+1);
-
+    // dice 1
     if (randomNumber1===1){
         document.querySelector(".img1").setAttribute("src","images/dice1.png");
     } 
@@ -21,6 +18,35 @@
     else if (randomNumber1===5){
         document.querySelector(".img1").setAttribute("src","images/dice5.png");
     } 
-    else {
+    else if (randomNumber1===6) {
         document.querySelector(".img1").setAttribute("src","images/dice6.png");
     }
+
+// dice 2
+    if (randomNumber2===1){
+        document.querySelector(".img2").setAttribute("src","images/dice1.png");
+    } 
+    else if (randomNumber2===2) {
+        document.querySelector(".img2").setAttribute("src","images/dice2.png");
+    } 
+    else if (randomNumber2===3){
+        document.querySelector(".img2").setAttribute("src","images/dice3.png");
+    } 
+    else if (randomNumber2===4){
+        document.querySelector(".img2").setAttribute("src","images/dice4.png");
+    } 
+    else if (randomNumber2===5){
+        document.querySelector(".img2").setAttribute("src","images/dice5.png");
+    } 
+    else if (randomNumber2===6){
+        document.querySelector(".img2").setAttribute("src","images/dice6.png");
+    }
+
+    // Change Header based on results of winner or draw
+ if (randomNumber1>randomNumber2){
+     document.querySelector("h1").innerHTML ="Player 1 wins!";
+ } else if (randomNumber1<randomNumber2){
+     document.querySelector("h1").innerHTML ="Player 2 wins!";
+ } else {
+     document.querySelector("h1").innerHTML ="Draw!"; 
+ }

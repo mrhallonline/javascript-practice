68377b58467aa1c4a888c390186e79fd362813dc
Kevin <mrhallonline@gmail.com>
2019-10-10 22:56:48 -0500

    starting fibonacci generator challenge using for loops

---
+++ fibonacciChallenge.js
@@ -0,0 +1,14 @@
+// Create a function that when called will create fibonnaci numbers
+// use fibonacciGenerator (n) 
+// where n is the number of items in the sequence
+// fibonacciGenerator (3) when called should equal [0,1,1] 
+// output should be an array for loop should use var i= 0 rather than i=0 for testing.
+
+function fibonacciGenerator (n) {
+    
+
+    
+    
+    //Return an array of fibonacci numbers.
+}
+

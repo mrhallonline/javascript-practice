04f4347cb375ab0f899f1874f3c680e5a8f6bded
Kevin <mrhallonline@gmail.com>
2019-10-10 21:54:27 -0500

    working on 99 bottles of beer song using while loops

---
+++ 99bottlesOfBeerChallenge.js
@@ -0,0 +1,13 @@
+//write code using while loop that replicates the 99 bottles of beer song.
+// 99 bottles of beer on the wall, 99 bottles of beer.
+// Take one down and pass it around, 98 bottles of beer on the wall.
+// No more bottles of beer on the wall, no more bottles of beer.
+// Go to the store and buy some more, 99 bottles of beer on the wall.
+var beerBottles = 99
+function beer() {
+
+    console.log(beerBottles + " bottles of beer on the wall, " + beerBottles + " bottles of beer.");
+    console.log("Take one down and pass it around, " + beerBottles + " bottles of beer on the wall");
+}
+
+beer();
